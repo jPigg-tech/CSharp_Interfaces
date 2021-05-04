cdb2e9367dfe4da5f3511baf635cbdeeaf3feab5
jeffpigg5@gmail.com <jeffpigg5@gmail.com>
2021-05-04 16:33:45 -0500

    Created an VideoEncoder class and implemented a IList of INotificationChannel objects for the class to encode while using the open close principal

---
+++ InterfacesAndPolymorphism/VideoEncoder.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace InterfacesAndPolymorphism
+{
+    public class VideoEncoder
+    {
+        private readonly IList<INotificationChannel> _notificationChannels;
+        public VideoEncoder()
+        {
+            _notificationChannels = new List<INotificationChannel>();
+        }
+
+        public void Encode(Video video)
+        {
+            // Video encodeing logic 
+            //...
+            foreach (var channel in _notificationChannels)
+            {
+                channel.Send(new Message());
+            }
+        }
+
+        public void RegisterNotificationChannel(INotificationChannel channel)
+        {
+            _notificationChannels.Add(channel);
+        }
+
+        // In this class its not talking about SMS, Emails we're 
+        // talking about abstarctions (INotificationChannel)
+        // With this we achieve Open Close Principal, our software is 
+        // open to extensions, we create new implementations of INotifacationChannel
+        // and closed for modification
+    }
+}
