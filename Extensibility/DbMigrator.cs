6e3217310b2a63054f815b02daf5d8361c42d5ce
jeffpigg5@gmail.com <jeffpigg5@gmail.com>
2021-05-04 11:41:57 -0500

    Implemented a new class, DbMigrator and passed the ILogger interface in its constructor to demostrate dependency injection in this class

---
+++ Extensibility/DbMigrator.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Extensibility
+{
+    class DbMigrator
+    {
+        private readonly ILogger _logger;
+        public DbMigrator(ILogger logger)
+        {
+            _logger = logger;
+        }
+        public void Migrate()
+        {
+            // Instead of Console.WriteLine we use the Ilogger
+            // interface for dependency injection and pass the message
+
+            _logger.LogInfo("Migration started at {0}" + DateTime.Now);
+            
+            // Details of migrating the database 
+
+            _logger.LogInfo("Migration started at {0}" + DateTime.Now);
+        }
+    }
+}
