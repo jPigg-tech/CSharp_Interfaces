2cd42f6558d8e4182a3348980546a72e114b4463
jeffpigg5@gmail.com <jeffpigg5@gmail.com>
2021-05-04 14:51:05 -0500

    Wrote a test in Progrm.cs to test if the FileLogger class is working properly

--- Extensibility/FileLogger.cs
+++ Extensibility/FileLogger.cs
@@ -1,12 +1,35 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace Extensibility
 {
-    class FileLogger
+    class FileLogger : ILogger
     {
+        private readonly string _path;
+        public FileLogger(string path)
+        {
+            _path = path;
+        }
+        public void LogError(string message)
+        {
+            Log(message, "ERROR");
+        }
+
+        public void LogInfo(string message)
+        {
+            Log(message, "INFO");
+        }
+
+        private void Log(string message, string messageType)
+        {
+            using (var streamWriter = new StreamWriter(_path, true))
+            {
+                streamWriter.WriteLine(messageType + ": " + message);
+            }
+        }
     }
 }
--- Extensibility/Program.cs
+++ Extensibility/Program.cs
@@ -10,7 +10,7 @@ namespace Extensibility
     {
         static void Main(string[] args)
         {
-            var dbMigrator = new DbMigrator(new ConsoleLogger());
+            var dbMigrator = new DbMigrator(new FileLogger("C:\\Users\\jeffp\\OneDrive\\Desktop\\CSharpExercises\\Interfaces\\log.txt"));
             dbMigrator.Migrate();
         }
     }
